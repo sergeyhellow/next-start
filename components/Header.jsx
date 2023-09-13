d2d2381a15175e174d31d13b786799ff58459a54
sergey <goblinsan@mail.ru>
2023-09-13 20:27:25 +0300

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import style from './Header.module.css';
+
+
+const
+    pages =[
+        {href:'/', title:'Home'},
+        {href:'/about', title: 'About' }
+    ];
+
+
+
+export default function Header()
+{
+    return <header className={style.header}>
+       <nav>
+        <ul>
+            {pages.map(({href,title})=> <li key={href} ><Link href={href} >{title}</Link>  </li>)}
+           { /*<li><Link href="/">Home</Link></li>
+            <li><Link href="/about">About</Link></li>*/}
+        </ul>
+        
+       </nav> 
+    </header>
+
+}
